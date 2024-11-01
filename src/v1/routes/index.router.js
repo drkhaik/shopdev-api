4e8815b9add34426f80c921252df3f45fdd0dc5f
trinhkhaiDev <khaitv21@uef.edu.vn>
2024-11-01 16:28:31 +0700

    FEAT: api users

--- src/v1/routes/index.router.js
+++ src/v1/routes/index.router.js
@@ -8,4 +8,25 @@ router.get('/checkstatus', (req, res, next) => {
     })
 })
 
+router.get('/api/users', (req, res, next) => {
+    res.status(200).json({
+        status: 'success',
+        message: 'api ok',
+        metadata: [
+            {
+                name: 'drkhaik',
+                age: 24
+            },
+            {
+                name: 'ronaldo',
+                age: 41
+            },
+            {
+                name: 'messi',
+                age: 39
+            }
+        ]
+    })
+})
+
 module.exports = router;
